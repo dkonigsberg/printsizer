fc0c6c9e7ad077ddb7923e6a00a7dbe6b43b11c1
Derek Konigsberg <dkonigsberg@logicprobe.org>
2020-07-28 15:06:07 -0700

    Added Github link to about fragment

--- app/src/main/java/org/logicprobe/printsizer/ui/AboutFragment.java
+++ app/src/main/java/org/logicprobe/printsizer/ui/AboutFragment.java
@@ -1,13 +1,17 @@
 package org.logicprobe.printsizer.ui;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,11 +21,13 @@ import org.logicprobe.printsizer.R;
 
 public class AboutFragment extends Fragment {
     private static final String TAG = AboutFragment.class.getSimpleName();
+    private static final String GITHUB_URL = "https://github.com/dkonigsberg/printsizer";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about, container, false);
         TextView textViewAppVersion = root.findViewById(R.id.textViewAppVersion);
+        ImageView imageViewGithub = root.findViewById(R.id.imageViewGithub);
 
         try {
             Context context = getContext();
@@ -34,6 +40,18 @@ public class AboutFragment extends Fragment {
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(TAG, e.toString());
         }
+
+        imageViewGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(GITHUB_URL));
+                    startActivity(browserIntent);
+                } catch (ActivityNotFoundException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
 
         return root;
     }
