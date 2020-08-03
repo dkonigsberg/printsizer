ebbb742ac9b280be8321f2b59a3a1206b94786e7
Derek Konigsberg <dkonigsberg@logicprobe.org>
2020-08-03 11:32:11 -0700

    Added Git-derived version marking into the app
    This affects the build process and the about screen

--- app/src/main/java/org/logicprobe/printsizer/ui/AboutFragment.java
+++ app/src/main/java/org/logicprobe/printsizer/ui/AboutFragment.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import org.logicprobe.printsizer.BuildConfig;
 import org.logicprobe.printsizer.R;
 
 public class AboutFragment extends Fragment {
@@ -27,18 +28,16 @@ public class AboutFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about, container, false);
         TextView textViewAppVersion = root.findViewById(R.id.textViewAppVersion);
+        TextView textViewAppDescribe = root.findViewById(R.id.textViewAppDescribe);
         ImageView imageViewGithub = root.findViewById(R.id.imageViewGithub);
 
-        try {
-            Context context = requireContext();
-            PackageManager packageManager = context.getPackageManager();
-            String packageName = context.getPackageName();
-            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
-            if (info.versionName != null && info.versionName.length() > 0) {
-                textViewAppVersion.setText(getString(R.string.label_version_prefix, info.versionName));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.e(TAG, e.toString());
+        textViewAppVersion.setText(getString(R.string.label_version_prefix, getVersionName()));
+
+        if (!isTaggedVersion()) {
+            textViewAppDescribe.setText(BuildConfig.VERSION_DESCRIBE);
+            textViewAppDescribe.setVisibility(View.VISIBLE);
+        } else {
+            textViewAppDescribe.setVisibility(View.GONE);
         }
 
         imageViewGithub.setOnClickListener(new View.OnClickListener() {
@@ -54,5 +53,34 @@ public class AboutFragment extends Fragment {
         });
 
         return root;
+    }
+
+    private String getVersionName() {
+        String versionName = null;
+        try {
+            Context context = requireContext();
+            PackageManager packageManager = context.getPackageManager();
+            String packageName = context.getPackageName();
+            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+            if (info.versionName != null && info.versionName.length() > 0) {
+                versionName = info.versionName;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, e.toString());
+        }
+        if (versionName == null) {
+            versionName = "vX.Y";
+        }
+        return versionName;
+    }
+
+    private boolean isTaggedVersion() {
+        String describe = BuildConfig.VERSION_DESCRIBE;
+        String parts[] = describe.split("-");
+        if (parts != null && parts.length > 2 && "0".equals(parts[1])) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
